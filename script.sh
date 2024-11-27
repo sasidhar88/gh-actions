11fb48575052a3e9ee82aaa5f37062225aec4e2f
Keerthipati Sasidhar Varma <95989168+sasidhar88@users.noreply.github.com>
2024-11-27 12:35:23 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,3 +15,37 @@ echo "CIDR Block: $CIDRBLOCK"
 echo "Private Key: $PRIVATE_KEY"
 echo "Public Key: $PUBLIC_KEY"
 echo "Group ID: $GROUP_ID"
+
+
+#Defining a function to setup values.
+setValues () {
+    
+    #: Defining MongoDB Staging cluster creds.
+    declare -g USERNAME=$MONGO_USER
+    declare -g PASSWORD=$MONGO_PASSWORD
+    #: Defing public key, private key and group id for the staging mongodb cluster
+    declare -g publickey=$PRIVATE_KEY
+    declare -g privatekey=$PUBLIC_KEY
+    #: Defining the cidr range for the staging vpc and nat gateway 
+    declare -g cidrblock=$CIDRBLOCK
+    declare -g natgw=$NATGW
+    declare -g groupid=$GROUP_ID
+    #: Defing source mongodb connection string
+    declare -g source_prod_url=$MONGO_CONNECTION_STRING_PROD_EU
+    declare -g mongodb_prod_user=$MONGO_USER_PROD_EU
+    declare -g mongodb_prod_pass=$MONGO_PASS_PROD_EU
+}
+setValues
+
+#: Proceesing the cidr block and natgw id
+processingCidrBlock () {
+    #: processing the cidr block and natgw.
+    declare -g cidrblock=$(echo "${!cidrblock}" | sed 's/%2F/\//g')
+    declare -g natgw=$(echo "${!natgw}" | sed 's/%2F/\//g')
+}
+
+processingCidrBlock
+
+echo "Pronting teh cidrblock and natgw"
+echo "$cidrblock"
+echo "$natgw"
