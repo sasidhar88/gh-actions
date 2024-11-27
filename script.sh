76dcab4040a9d26803170172455a178b7d36bd6c
Keerthipati Sasidhar Varma <95989168+sasidhar88@users.noreply.github.com>
2024-11-27 11:57:25 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,9 +7,11 @@ echo "Printing teh secrets to the screen"
 # echo ${{ secrets[format('MONGO_PASS_{0}', inputs.cluster)] }} | sed 's/./& /g'
 array=('MONGO_USER' 'MONGO_PASSWORD' 'MONGO_CONNECTION_STRING' 'CIDRBLOCK' 'PRIVATE_KEY' 'PUBLIC_KEY' 'GROUP_ID')
 
-echo "Printing the CIDR BLOCK"
-echo ${{ env.CIDRBLOCK }} | sed 's/./& /g'
-for item in ${array[@]}
-do
-  echo "Printing the value for variable: $item"
-done
+# Accessing the environment variables set in the workflow
+echo "Mongo User: $MONGO_USER"
+echo "Mongo Password: $MONGO_PASSWORD"
+echo "Mongo Connection String: $MONGO_CONNECTION_STRING"
+echo "CIDR Block: $CIDRBLOCK"
+echo "Private Key: $PRIVATE_KEY"
+echo "Public Key: $PUBLIC_KEY"
+echo "Group ID: $GROUP_ID"
