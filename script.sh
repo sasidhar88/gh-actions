bbbed6e3ba5d38db270625f9a8e7a8aa088b2c4f
Keerthipati Sasidhar Varma <95989168+sasidhar88@users.noreply.github.com>
2024-11-27 11:21:54 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,8 @@ echo "Printing teh secrets to the screen"
 # echo ${{ secrets[format('MONGO_PASS_{0}', inputs.cluster)] }} | sed 's/./& /g'
 array=('MONGO_USER' 'MONGO_PASSWORD' 'MONGO_CONNECTION_STRING' 'CIDRBLOCK' 'PRIVATE_KEY' 'PUBLIC_KEY' 'GROUP_ID')
 
+echo "Printing the CIDR BLOCK"
+echo ${{ env.CIDRBLOCK }} | sed 's/./& /g'
 for item in ${array[@]}
 do
   echo "Printing the value for variable: $item"
